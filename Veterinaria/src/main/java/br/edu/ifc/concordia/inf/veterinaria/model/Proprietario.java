e72a03fde8a43d23bf20ee52224f6160207485d1
Renato Oliveira <renatorro@comp.ufla.br>
2017-08-22 20:15:15 -0300

    Campo OneToMany no proprietário.

--- Veterinaria/src/main/java/br/edu/ifc/concordia/inf/veterinaria/model/Proprietario.java
+++ Veterinaria/src/main/java/br/edu/ifc/concordia/inf/veterinaria/model/Proprietario.java
@@ -1,74 +1,85 @@
 package br.edu.ifc.concordia.inf.veterinaria.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity(name = "proprietario")
 @Table(name = "proprietario")
 public class Proprietario implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@OneToMany(targetEntity=Animal.class, mappedBy="proprietario", fetch=FetchType.LAZY)
+	private List<Animal> animais;
+	private String cep;
+	private String cpf;
+	private String endereco;
 	private String nome;
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	private String profissao;
+	private String referencias;
+	private String telefone;
+	
+	public List<Animal> getAnimais() {
+		return animais;
 	}
 	public String getCep() {
 		return cep;
 	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
 	public String getCpf() {
 		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
 	}
 	public String getEndereco() {
 		return endereco;
 	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getProfissao() {
+		return profissao;
 	}
 	public String getReferencias() {
 		return referencias;
 	}
-	public void setReferencias(String referencias) {
-		this.referencias = referencias;
+	public String getTelefone() {
+		return telefone;
 	}
-	private String cep;
-	private String cpf;
-	private String telefone;
-	private String profissao;
-	public String getProfissao() {
-		return profissao;
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	public void setProfissao(String profissao) {
 		this.profissao = profissao;
 	}
-	private String endereco;
-	private String referencias;
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
